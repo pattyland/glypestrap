2f268c26ac1c77e0f6ba41790318817375848d1a
pattyland <mail@pattyland.de>
2014-01-18 12:29:10 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+/*******************************************************************
+* Glype Proxy Script
+* 
+* This theme configuration file allows easy customization of the
+* theme without editing the HTML templates.
+*******************************************************************
+* Theme: glypestrap
+* Author: pattyland
+* Website: http://pattyland.de/
+******************************************************************/
+
+/*****************************************************************
+* Themes can use "theme replacements". These are HTML tags of the format
+* <!--[tag_name]--> in the template files. To automatically replace
+* these placeholders with other text, use the $themeReplace array.
+*  e.g.
+* <!--[tag_name]--> will be replaced with the value of $themeReplace['tag_name']
+******************************************************************/
+
+// Website name
+$themeReplace['site_name'] = 'proxy';
+
+//Ad
+$themeReplace['ad'] = '';
+
