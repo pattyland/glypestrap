be9fe0107f7fc52a3899cb1ec3e3c6fddb9b57ad
pattyland <mail@pattyland.de>
2014-08-17 19:15:02 +0200

    removed developer tipps

--- config.php
+++ config.php
@@ -10,14 +10,6 @@
 * Website: http://pattyland.de/
 ******************************************************************/
 
-/*****************************************************************
-* Themes can use "theme replacements". These are HTML tags of the format
-* <!--[tag_name]--> in the template files. To automatically replace
-* these placeholders with other text, use the $themeReplace array.
-*  e.g.
-* <!--[tag_name]--> will be replaced with the value of $themeReplace['tag_name']
-******************************************************************/
-
 // Website name
 $themeReplace['site_name'] = 'proxy';
 
